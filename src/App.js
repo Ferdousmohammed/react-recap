dc59a007df55b0ff27c4e53af193715ac388305d
Ferdousmohammed <ferdousmohammed29@gmail.com>
2023-06-05 23:31:51 +0600

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import React from 'react';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with my-recapss
+          Learn React with my-recaps
         </a>
        <div>
        <Tahsan name = 'JayedBro'></Tahsan>
@@ -27,6 +28,8 @@ function App() {
         <Counter> </Counter>
         <Users></Users>
         <Users></Users>
+        <MrChoplu></MrChoplu>
+        <CountryList></CountryList>
 
        </div>
       </header>
@@ -44,7 +47,64 @@ function Tahsan(props) {
   )
 }
 
- 
+function MrChoplu () {
+  const [chuppu, pressident] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => pressident(data))
+  })
+  return (
+    <div style={{backgroundColor: 'grey', padding: '15px', margin: '20px',borderRadius: '12px', color: ''}}>
+        <p> This is our new president mr choplus Biography{chuppu.length} </p>
+        <ul>
+          {
+            chuppu.map(cc =><li>{cc.email} </li> )
+          }
+        </ul>
+    </div>
+  )
+}
+
+const CountryList = () => {
+  const data = [
+    {
+      name: {
+        common: 'Jordan',
+        official: 'Hashemite Kingdom of Jordan',
+        nativeName: {
+          ara: {
+            official: 'المملكة الأردنية الهاشمية',
+            common: 'الأردن'
+          }
+        }
+      },
+      tld: '.jo',
+      population: 10203140,
+      flag: 'https://example.com/flag.jpg',
+      area: 89342
+    }
+    // Add more country objects here if needed
+  ];
+
+  return (
+    <div style={{backgroundColor: 'green', padding: '15px', margin: '20px',borderRadius: '12px', color: 'red'}}>
+      <h1>Country List</h1>
+      <ul>
+        {data.map((country, index) => (
+          <li key={index}>
+            <h2>{country.name.common}</h2>
+            <p>Population: {country.population}</p>
+            <img src={country.flag} alt="Flag" />
+            <p>Area: {country.area}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
  
 
 function Counter(props) {
